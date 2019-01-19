9f8bc7ea73daa8d4907f78d8c7a939f919390a85
wkalina88 <wkalina88@gmail.com>
2019-01-19 15:17:12 +0100

    Last update

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -6,7 +6,9 @@ public class EnemyController : MonoBehaviour {
 
    
     private Animator animator;
-    
+
+    public float damageDistance;
+
     public float attackRange;
     
     private float lastAttackTime;
@@ -21,7 +23,8 @@ public class EnemyController : MonoBehaviour {
     // Use this for initialization
     void Start () {
         animator = GetComponent<Animator>();
-        
+        //damageDistance = 1.85f;
+        target = GameObject.Find("Player").gameObject.GetComponent<PlayerMovement>().transform;
 	}
 	
 	// Update is called once per frame
@@ -33,22 +36,23 @@ public class EnemyController : MonoBehaviour {
 
 
         //move towards the player
-        if (Vector3.Distance(transform.position, target.position) > 2f)
+        if (Vector3.Distance(transform.position, target.position) > damageDistance + 4f)
         {//move if distance from target is greater than 1
             transform.Translate(new Vector3(speed * Time.deltaTime, 0, 0));
             animator.SetBool("isMoving", true);
-            
+        }
+        else
+        {
+            animator.SetBool("isHitting", true);
         }
 
-       
     }
     void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Player")
         {
             //animator.SetBool("isMoving", false);
-            animator.SetBool("isHitting", true);
-
+            //animator.SetBool("isHitting", true);
         }
 
     }
